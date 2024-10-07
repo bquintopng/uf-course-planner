a1ccecd7a79e17324e9b5f9921605606f72d59b7
ryanlu-dev <ryan@ryanlu.dev>
2024-10-07 12:49:15 -0400

    test db-react

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div><h1>Hello {value}</h1></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
